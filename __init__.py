6fd1de2a14cca23a986915a0566070d7acd0a7a3
Kori, Suma <suma.kori93@gmail.com>
2020-11-11 19:14:53 +0100

    initial commit init.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding=UTF-8
+# Written by Suma Kori
+
+"""
+- author: Suma Kori
+- e-mail: suma.kori93@gmail.com
+"""
+
+try:
+    from MergeOverlappingIntervals import merge_intervals
+except:
+    print("Unable to import")
